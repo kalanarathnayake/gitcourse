c42ecec0d651ade5b07a3171d8a5d05abea2f18d
kalanaRathnayake <kalanagayanga8@cf.lk>
2025-07-24 11:41:28 +0530

    added my latest update to the project

--- hello.js
+++ hello.js
@@ -2,11 +2,17 @@ console.log("git commit hello world");
 console.log("hello from the master branch");
 console.log("hello from the resetcheck feature branch");
 console.log("this is a new line added to the file");
-
 console.log("this is a new line added to the file");
 
-function add(){
+function add() {
     console.log("this is a new line added to the file");
 }
 // done the extra work
 // this is a new line added to the file
+// add a new function to greet a user
+
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+// Example usage:
+greet("Kalana");
