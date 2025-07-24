6a5fa406ffb1794b0e0c1f8786391e32e02d0113
kalanaRathnayake <kalanagayanga8@cf.lk>
2025-07-24 11:25:11 +0530

    add another console log ater

--- hello.js
+++ hello.js
@@ -3,3 +3,4 @@ console.log("hello from the master branch");
 console.log("hello from the resetcheck feature branch");
 console.log("this is a new line added to the file");
 console.log("lets try out  revert command in git");
+console.log("this is another new line added to the file");
