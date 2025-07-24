66fb182d4da5ac67653815929f3d240061c7d323
kalanaRathnayake <kalanagayanga8@cf.lk>
2025-07-24 11:24:14 +0530

    checking the revert command

--- hello.js
+++ hello.js
@@ -2,3 +2,4 @@ console.log("git commit hello world");
 console.log("hello from the master branch");
 console.log("hello from the resetcheck feature branch");
 console.log("this is a new line added to the file");
+console.log("lets try out  revert command in git");
