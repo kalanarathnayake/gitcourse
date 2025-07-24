c640bbbf2b370e8829e2ee862ff1879836bd0f04
kalanaRathnayake <kalanagayanga8@cf.lk>
2025-07-24 11:07:38 +0530

    removed the buggy code from the whole project

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
 console.log("git commit hello world");
 console.log("hello from the master branch");
 console.log("hello from the resetcheck feature branch");
+console.log("this is a new line added to the file");
