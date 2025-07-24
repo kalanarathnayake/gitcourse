001712280911c5a9e7c22cdb1da53bc1ad3666cd
kalanaRathnayake <kalanagayanga8@cf.lk>
2025-07-24 11:38:20 +0530

    saved the damn day for boss

--- hello.js
+++ hello.js
@@ -2,3 +2,11 @@ console.log("git commit hello world");
 console.log("hello from the master branch");
 console.log("hello from the resetcheck feature branch");
 console.log("this is a new line added to the file");
+
+console.log("this is a new line added to the file");
+
+function add(){
+    console.log("this is a new line added to the file");
+}
+// done the extra work
+// this is a new line added to the file
