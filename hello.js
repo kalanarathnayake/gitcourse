147518f6b542455355f0b197f4363ba862f4f6b5
kalanaRathnayake <kalanagayanga8@cf.lk>
2025-07-24 11:28:03 +0530

    reverted the code

--- hello.js
+++ hello.js
@@ -2,5 +2,3 @@ console.log("git commit hello world");
 console.log("hello from the master branch");
 console.log("hello from the resetcheck feature branch");
 console.log("this is a new line added to the file");
-console.log("lets try out  revert command in git");
-console.log("this is another new line added to the file");
